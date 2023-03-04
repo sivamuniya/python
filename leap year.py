282439d69e0ab8e718a7de4476e801de29d047ea
sivamuniya <126935685+sivamuniya@users.noreply.github.com>
2023-03-04 10:25:57 +0530

    Add files via upload

---
+++ leap year.py
@@ -0,0 +1,12 @@
+Python 3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+======================================================================= RESTART: C:/Users/student/eligibility for vote.py ======================================================================
+enter age 19
+eligible for vote
+>>> 
+======================================================================= RESTART: C:/Users/student/eligibility for vote.py ======================================================================
+it is lesp year
+>>> 
+======================================================================= RESTART: C:/Users/student/eligibility for vote.py ======================================================================
+it is leap year
