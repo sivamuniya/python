2b4d13dc4f49fae9f48ac846ab800574d0ec6e2a
sivamuniya <126935685+sivamuniya@users.noreply.github.com>
2023-03-04 10:39:30 +0530

    Add files via upload

---
+++ area of circle.py
@@ -0,0 +1,25 @@
+Python 3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+======================================================================= RESTART: C:/Users/student/eligibility for vote.py ======================================================================
+enter age 19
+eligible for vote
+>>> 
+======================================================================= RESTART: C:/Users/student/eligibility for vote.py ======================================================================
+it is lesp year
+>>> 
+======================================================================= RESTART: C:/Users/student/eligibility for vote.py ======================================================================
+it is leap year
+>>> 
+======================================================================= RESTART: C:/Users/student/eligibility for vote.py ======================================================================
+c is greater
+>>> 
+======================================================================= RESTART: C:/Users/student/eligibility for vote.py ======================================================================
+Traceback (most recent call last):
+  File "C:/Users/student/eligibility for vote.py", line 2, in <module>
+    r=print(float("enter radius of circle"))
+ValueError: could not convert string to float: 'enter radius of circle'
+>>> 
+======================================================================= RESTART: C:/Users/student/eligibility for vote.py ======================================================================
+enter radius of circle5
+78.5
